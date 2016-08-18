075c8c1d8727645e00713f7dcb8f5c89c78541df
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-08-18 18:23:25 -0400

    Assignment-22search: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -43,6 +43,16 @@ class AddressBook
      end
    end
 
+   def iterative_search(name)
+     @entries.each do |entry|
+       if entry.name == name
+         return entry
+       end
+     end
+
+     return nil
+   end
+
    # search AddressBook for a specific entry by name
    def binary_search(name)
 
